d574dc378d218d56019580858fe3c258875ae6a4
Pierre Defourneaux <pierre.defourneaux@gmail.com>
2026-03-18 09:51:03 +0100

    Sync

--- yolo_data/train_ultralytics_old_but_good_working.py
+++ yolo_data/train_ultralytics_old_but_good_working.py
@@ -13,7 +13,7 @@ def main():
         workers=1,
         project="badminton_project",
         name="run_v1",
-        # Augmentations agressives pour éviter l'overfitting
+        # Augmentations pour éviter l'overfitting
         degrees=10.0,      # Rotation légère (très important si la caméra bouge)
         scale=0.5,         # Zoom in/out (force à reconnaitre des terrains plus petits/grands)
         mosaic=1.0,        # Mélange 4 images (excellent pour YOLO)
@@ -21,9 +21,10 @@ def main():
         hsv_s=0.4,         # Variation de saturation (lumière changeante)
         hsv_v=0.4,         # Variation de luminosité
         fliplr=0.5,        # Miroir horizontal (le terrain est symétrique gauche/droite)
-        flipud=0.0,        # JAMAIS de miroir vertical pour le badminton (le haut et le bas sont différents en perspective)
+        flipud=0.0,        # Pas de miroir vertical pour le badminton (le haut et le bas sont différents en perspective)
         save=True
     )
 
 if __name__ == "__main__":
     main()
+    
